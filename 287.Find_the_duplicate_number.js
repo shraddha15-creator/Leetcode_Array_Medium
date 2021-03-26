72cd1f7eb709cf38f48ab6565f6abe6b8a81d1c5
Shraddha Muley <66071175+shraddha15-creator@users.noreply.github.com>
2021-03-26 14:12:01 +0530

    Create 287.Find_the_duplicate_number.js

---
+++ 287.Find_the_duplicate_number.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findDuplicate = function(nums) {
+    let set = new Set();
+    for(let i = 0; i < nums.length; i++){
+        if(set.has(nums[i])){
+            return nums[i];
+        }else{
+            set.add(nums[i]);
+        }
+    }
+};
